a704b974d7e734d698c8446552e4cd1b015e3e16
Siddhartha <siddureddyvr1115@email.com>
2026-04-03 10:04:30 +0530

    feat: add PropTypes validation to Navbar and CartDrawer

--- client/src/components/CartDrawer.jsx
+++ client/src/components/CartDrawer.jsx
@@ -2,6 +2,7 @@
 import { useEffect } from "react";
 import { fmt } from "../utils/formatters";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 function CartDrawer({
   isOpen,
@@ -228,3 +229,22 @@ function CartDrawer({
 }
 
 export default CartDrawer;
+
+CartDrawer.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  cart: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      name: PropTypes.string,
+      brand: PropTypes.string,
+      price: PropTypes.number,
+      image: PropTypes.string,
+      qty: PropTypes.number,
+    })
+  ).isRequired,
+  cartCount: PropTypes.number.isRequired,
+  cartTotal: PropTypes.number.isRequired,
+  updateQty: PropTypes.func.isRequired,
+  removeFromCart: PropTypes.func.isRequired,
+};
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "../auth/firebase";
 import { useAuth } from "../auth/useAuth";
+import PropTypes from "prop-types";
 
 export default function Navbar({
   variant = "landing",
@@ -231,3 +232,14 @@ export default function Navbar({
     </nav>
   );
 }
+
+Navbar.propTypes = {
+  variant: PropTypes.string,
+  navOpaque: PropTypes.bool,
+  onSearchToggle: PropTypes.func,
+  cartCount: PropTypes.number,
+  onCartOpen: PropTypes.func,
+  menuOpen: PropTypes.bool,
+  setMenuOpen: PropTypes.func,
+  onSignOut: PropTypes.func,
+};
